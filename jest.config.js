244e992552dc9e3af5813cb9b988ab2a2b8274fe
Romain <asyncadventures@gmail.com>
2019-07-10 21:27:33 +0200

    Split unit and integration tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  rootDir: '.',
+  collectCoverage: true,
+  collectCoverageFrom: ['lib/**/*.js'],
+  projects: [
+    {
+      displayName: 'unit',
+      testMatch: ['<rootDir>/test/unit/**/*.test.js']
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['<rootDir>/test/integration/**/*.test.js']
+    }
+  ]
 }
