a4596cfd68489e3f94f14631d7b5746ba6796269
Romain <rprieto@users.noreply.github.com>
2021-04-05 23:52:16 +0200

    test: move HEIC tests to their own file

--- test/integration/image.test.js
+++ test/integration/image.test.js
@@ -156,46 +156,6 @@ describe('image', () => {
     }, done)
   })
 
-  it('can process a single-image HEIC file', done => {
-    diff.image({
-      input: 'images/heic-single.heic',
-      expect: 'images/heic-single.jpg',
-      options: {
-        height: 300
-      }
-    }, done)
-  })
-
-  it('can process a burst-image HEIC file', done => {
-    diff.image({
-      input: 'images/heic-burst.heic',
-      expect: 'images/heic-burst.jpg',
-      options: {
-        height: 300
-      }
-    }, done)
-  })
-
-  it('can process a live (photo + video) HEIC file', done => {
-    diff.image({
-      input: 'images/heic-live.heic',
-      expect: 'images/heic-live.jpg',
-      options: {
-        height: 300
-      }
-    }, done)
-  })
-
-  it('can process a HEIC with a P3 color profile', done => {
-    diff.image({
-      input: 'images/heic-color-profile.heic',
-      expect: 'images/heic-color-profile.jpg',
-      options: {
-        height: 300
-      }
-    }, done)
-  }, 10000)
-
   const ORIENTATIONS = [1, 2, 3, 4, 5, 6, 7, 8]
 
   ORIENTATIONS.forEach((orientation) => {
